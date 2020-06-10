7669f87c18df6e95546ab383e0f0b93e839ef56a
hugo <hugo.delacruz@tecsup.edu.pe>
2020-06-09 21:18:13 -0500

    commit Hugo Final

---
+++ semana_lab10/semana_lab10/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace semana_lab10
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            viewtoview.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+            };
+            binding.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            listview.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+            picker.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
